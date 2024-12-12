d775778cb57eeb2484347a7b7a116268cde699ef
jchassaing <jeremie.chassaing@gmail.com>
2024-12-12 22:46:59 +0100

    More functional day12

--- day12.fsx
+++ day12.fsx
@@ -8,6 +8,9 @@ let lw = w+1
 let h = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).Length
 
 let toPos index = index % lw, index / lw
+let toIndex (x,y) = x + y * lw
+
+let inBounds (x,y) = x >= 0 && x < w && y >= 0 && y < h
 
 let (++) (x1,y1) (x2,y2) = x1+x2,y1+y2
 let N = 0,-1
@@ -20,26 +23,28 @@ let rotL (x,y) = y,-x
 
 let inv (x,y) = -x,-y
 
-let inline get (x,y) =
-   if x >= 0 && x < w && y >= 0 && y < h then 
-        input[x + lw * y]
+let inline get p =
+   if inBounds p then 
+        input[toIndex p]
    else
         '.'
 
 /// A map with deduplicated chars
 let map  = Array.replicate  (lw*h) '\000'
 
-let mget  (x,y) = 
-    if x >= 0 && x < w && y >= 0 && y < h then
-        map[x+y*lw]
+let mget  p = 
+    if inBounds p then
+        map[toIndex p]
     else
         '\000'
-let mset (x,y) v = map[x+y*lw] <- v
+let mset p v = map[toIndex p] <- v
+
+// place line ends (just for print)
 for y in 0 .. h-1 do
     mset(w,y) '\n' 
 
 
-let push b h t = if b then h :: t else t
+let pushIf b h t = if b then h :: t else t
 
 // fills area containing c with value v (first position should be on stack)
 let rec fill c v stack =
@@ -55,10 +60,10 @@ let rec fill c v stack =
             // push all neighbors on stack
             let stack =
                 rest
-                |> push (c = get (p++N)) (p++N)
-                |> push (c = get (p++E)) (p++E) 
-                |> push (c = get (p++S)) (p++S) 
-                |> push (c = get (p++W)) (p++W) 
+                |> pushIf (c = get (p++N)) (p++N)
+                |> pushIf (c = get (p++E)) (p++E) 
+                |> pushIf (c = get (p++S)) (p++S) 
+                |> pushIf (c = get (p++W)) (p++W) 
                 
             fill c v stack
 
@@ -80,119 +85,132 @@ String map |> printfn "%s"
 // count cells for each area, and keep them
 let areas =
     map
-    |> Array.filter (fun c -> c <> '\n')
-    |> Array.countBy id
-    |> dict
+    |> Seq.filter ((<>) '\n')
+    |> Seq.countBy id
+    |> Map.ofSeq
 
 
     
 open System.Collections.Generic
 // all perimeters lengths by key
-let perimeters = Dictionary<char, int>()
+
 // increment perimeter for key
-let add c = 
-    let v = 
-        match perimeters.TryGetValue(c) with
-        | true, v -> v+1
-        | false,_ -> 1
-    perimeters[c] <- v
+let addIf cond c perimeters = 
+    if cond then
+        let v = 
+            match Map.tryFind c perimeters with
+            | Some v -> v+1
+            | None -> 1
+        Map.add c v perimeters
+    else
+        perimeters
 
 // for each cell, look neigboors and increment perimeter in each direction if not same area
-for y in 0 ..h-1 do
-    for x in 0..h-1 do
-        let p = x,y
-        let c = mget (x,y)
-        if c <> mget(p++N) then add c
-        if c <> mget(p++E) then add c
-        if c <> mget(p++S) then add c
-        if c <> mget(p++W) then add c
+let perimeters= 
+    let positions = seq {
+        for y in 0 ..h-1 do
+            for x in 0..h-1 do
+                x,y }
+    (Map.empty, positions)
+    ||> Seq.fold (fun perimeters p ->
+            let c = mget p
+            perimeters
+            |> addIf (c <> mget(p++N)) c 
+            |> addIf (c <> mget(p++E)) c
+            |> addIf (c <> mget(p++S)) c
+            |> addIf (c <> mget(p++W)) c
+    )
 
 
 String map |> printfn "%s"
 
 // Part 1
-perimeters.Keys
-    |> Seq.sumBy(fun k -> areas[k] * perimeters[k])
+Map.keys perimeters
+|> Seq.sumBy(fun k -> areas[k] * perimeters[k])
 
 // find the external sides of area filled with key c, starting from start
 // remove neighbors from set as we find them on the left
 // as we find first occurence of key to start, it's alwas in a top left border, so
 // we can search starting east, and finish when reaching start with direction north
-let rec extSides c start p dir borders (neis: HashSet<_>) =
+let rec extSides c start p dir borders neighbors =
     if p = start && dir = N then
         // finished, just remove left cell from neighbors
         let l = rotL dir
-        neis.Remove(mget (p++l)) |> ignore
-        borders
+        borders, Set.remove (mget (p++l)) neighbors
     else
         let l = rotL dir
         let r = rotR dir
         // remove left cell from neighbors
         let lc = mget (p++l)
-        neis.Remove(lc) |> ignore
+        let neighbors =  Set.remove lc neighbors
 
         if c = lc then
             // left cell is in area, turn left and move to this cell
             // were changing border
-            extSides c start (p++l) l (borders+1) neis
+            extSides c start (p++l) l (borders+1) neighbors
         elif c = mget (p++dir) then
             // next cell is area, continue straight, stay on same border
-            extSides c start (p++dir) dir borders  neis
+            extSides c start (p++dir) dir borders  neighbors
         else 
             // we have to turn right, this is a new border
-            extSides c start p r (borders+1) neis
+            extSides c start p r (borders+1) neighbors
 
 
 // find inner sides of area of key c
 // same thing as before we start from top left
-let rec innerSides c start p dir borders (neis: HashSet<_>) =
+let rec innerSides c start p dir borders neighbors =
     if p = start && dir = N then
         // 
-        borders
+        borders, neighbors
     else
         let l = rotL dir
         let r = rotR dir
         // remove current cell from neighbors (there can be 2 adjacents area inside an outside area)
-        neis.Remove(mget p) |> ignore
+        let neighbors = Set.remove (mget p) neighbors
         if c <> mget (p++l)  then
             // the area continues on the left, new border
-            innerSides c start (p++l) l (borders+1) neis
+            innerSides c start (p++l) l (borders+1) neighbors
         elif c <> mget (p++dir) && mget(p++dir) <> '\000' then
             // the border is straight 
-            innerSides c start (p++dir) dir borders neis
+            innerSides c start (p++dir) dir borders neighbors
         else 
             // we need to turn right, new border
-            innerSides c start p r (borders+1) neis
+            innerSides c start p r (borders+1) neighbors
 
 // find all neighbors for a given area
 let neighbors c =
-    let neis = HashSet()
-    for y in 0 .. h do
-        for x in 0 ..w do 
-            let p = x,y
+    let neighbors = Set.empty
+    let positions = seq {
+        for y in 0 .. h do
+            for x in 0 ..w do 
+                x,y  }
+    (neighbors, positions)
+    ||> Seq.fold (fun neighbors  p   ->
             if mget p = c then
-                neis.Add(mget (p++N)) |> ignore
-                neis.Add(mget (p++E)) |> ignore
-                neis.Add(mget (p++S)) |> ignore
-                neis.Add(mget (p++W)) |> ignore
-    neis.Remove('\000') |> ignore
-    neis.Remove(c) |> ignore
-    neis
+                neighbors
+                |> Set.add (mget (p++N))
+                |> Set.add (mget (p++E))
+                |> Set.add (mget (p++S)) 
+                |> Set.add (mget (p++W))
+            else
+                neighbors)
+    |> Set.remove('\000') 
+    |> Set.remove(c)
 
-// count total in side for area k (find unexplored holes in neis)
-let rec inSides k (neis: _ HashSet) total =
-    if neis.Count = 0 then
+// count total in side for area k (find unexplored holes in neighbors)
+let rec inSides k neighbors  total =
+    if Set.isEmpty neighbors then
         // all inner neighbors area have been explored 
         total
     else
         // next inner neighbor to explor
-        let c = neis |> Seq.head 
+        let c = neighbors |> Seq.head 
         // top left point
         let p = map |> Array.findIndex((=)c) |> toPos 
         // find sides of hole, remove neighbors while explored
-        let s = innerSides k p p E 1 neis
+        let s, neighbors = innerSides k p p E 1 neighbors
         // try find remaining holes
-        inSides k neis (total+s)
+        inSides k neighbors (total+s)
         
 
 // compute the total number of sides
@@ -200,11 +218,11 @@ let rec inSides k (neis: _ HashSet) total =
 // then count external sides, removing external neighbors
 // find holes sides using remaining internal neighbors
 let sides c = 
-    let neis = neighbors c
+    let neighbors = neighbors c
     let i = map |> Array.findIndex((=) c)
     let p = toPos i
-    let s = extSides c p p E 1 neis
-    let is = inSides c neis 0
+    let s, neighbors = extSides c p p E 1 neighbors
+    let is = inSides c neighbors 0
     s + is
 
 String map
@@ -212,6 +230,5 @@ String map
 // Part 2
 areas.Keys
 |> Seq.sumBy (fun c ->
-    printfn "%c" c
     let s = sides c
     s * areas[c])
