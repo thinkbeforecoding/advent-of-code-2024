28de5f9ffb4dfeed71c986523163329613b16286
jchassaing <jeremie.chassaing@gmail.com>
2024-12-25 14:51:46 +0100

    Comment day10

--- day10.fsx
+++ day10.fsx
@@ -1,23 +1,26 @@
+// --- Day 10: Hoof It ---
+// https://adventofcode.com/2024/day/10
+
 open System
 
+// read the map as a string
 let input = IO.File.ReadAllText("input/day10.txt").ReplaceLineEndings("\n")
-// let input = 
-//     """..90..9
-// ...1.98
-// ...2..7
-// 6543456
-// 765.987
-// 876....
-// 987...."""
 
+// the width of the map
 let w = input.IndexOf('\n')
+// the distance to next line
 let lw = w+1
+// the height of the map
 let h = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).Length
 
+// check if x,y is in bounds
 let inbounds (x,y) = x >= 0 && x < w && y >= 0 && y < h
+// convert x,y to string index
 let toIndex (x,y) = x+y*lw
+// convert string index to x,y
 let toPos index = index%lw, index / lw
 
+// get character at position x,y, return '.' if out of map
 let get p = 
     if inbounds p then
         input[toIndex p]
@@ -25,25 +28,29 @@ let get p =
         '.'
 
 
-let rec findHeads index  all =
-    if index < input.Length then
-        let c = input[index]
-        if c = '0' then
-            findHeads (index+1) (toPos index :: all)
-        else
-            findHeads (index+1) all
-    else
-        all
-        
+// get all heads
+// find the '0's and convert index to x,y 
+let heads =
+    input
+    |> Seq.mapi (fun i c -> if c = '0' then Some(toPos i) else None)
+    |> Seq.choose id
+    |> Seq.toList
 
-findHeads 0 []
-
+// add vectors
 let (++) (x1,y1) (x2,y2) = x1+x2, y1+y2
 
+// the 4 main directions
 let N = 0,-1
 let E = 1,0
 let S = 0,1
 let W = -1,0
+
+// find the next direction
+// c is the current trail char (starts at '0')
+// pos is the x,y position on the map,
+// dir is the direction to test
+// adds the next position in given direction to the stack if
+// it follows current char
 let nextDir c pos dir stack =
     let n = pos ++ dir
     let nc = get n
@@ -51,30 +58,49 @@ let nextDir c pos dir stack =
         n :: stack
     else
         stack
+// explor trails for part 1 and 2 
+// ends contains the all the ends, paths contains the path count
 let rec trail' stack ends paths =
     match stack with
-    | [] -> ends,paths
+    | [] ->
+        // we finished the exploration
+        // returns found ends ands paths
+        ends,paths
     | pos :: tail ->
+        // test the trail at position
+        // get the character there
         let c = get pos
         
         if c = '9' then
+            // its a 9, we found an end,
+            // test others trails/path, but add this to the ends, and add one path to total
             trail' tail (Set.add pos ends) (paths+1)
         else
+            // this is not a 9,
+            // explore the 4 directions, and add them to stack for further exploration if they
+            // contain current char + 1
             let stack =
                 tail
                 |> nextDir c pos N 
                 |> nextDir c pos E
                 |> nextDir c pos S
                 |> nextDir c pos W
+
+            // continue exploration
             trail' stack ends paths
 
+// call the recursive function with start parameters
 let trails pos = trail' [pos] Set.empty 0
             
 
-findHeads 0 [] 
+// Part 1
+// sum count of ends for all heads
+heads 
 |> List.sumBy (trails >> fst >> Set.count) 
 
-findHeads 0 [] 
+// Part 2
+// sum path count for all heads
+heads
 |> List.sumBy (trails >> snd ) 
 
 
