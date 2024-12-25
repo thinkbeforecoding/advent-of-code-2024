bce0aa0a87d23390b00fe2338fcf1cdf3f118441
jchassaing <jeremie.chassaing@gmail.com>
2024-12-25 13:05:02 +0100

    Comment day2

--- day02.fsx
+++ day02.fsx
@@ -1,5 +1,6 @@
 open System
 
+// parse input as array of int[]
 let input =
     IO.File.ReadAllLines("input/day02.txt")
     |> Array.map (fun l ->
@@ -7,18 +8,24 @@ let input =
         |> Array.map int 
     )
 
+// check if all values are >0 and <=3
 let pos diffs =
     diffs |> Array.forall (fun l -> l > 0 && l <= 3)
+// check if all values are >=-3 and < 0 
 let neg diffs =
     diffs |> Array.forall (fun l -> l < 0 && l >= -3)
 
+// check if safe
 let safe report= 
+    // the differences between consecutives numbers
     let diffs = 
         report
         |> Array.pairwise
         |> Array.map (fun (x,y) -> y-x)
+    // check if pos or neg applies
     pos diffs || neg diffs
 
+// count reports that are sage
 input
 |> Array.filter safe
 |> Array.length
@@ -26,16 +33,19 @@ input
 
 // Part 2
 
+// if the report is true, no problem,
+// otherwhise, test if removing at any position can make it safe
 let dampenerSafe report =
     if safe report then
         true
     else
-        [0 .. Array.length report - 1] 
-        |> List.exists (fun i ->
+        {0 .. Array.length report - 1} 
+        |> Seq.exists (fun i ->
             report
             |> Array.removeAt i
             |> safe)
 
+// count safe inputs
 input
 |> Array.filter dampenerSafe
 |> Array.length
