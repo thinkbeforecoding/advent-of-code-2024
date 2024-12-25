25b6c88169b1e787aa2a6e05d0c4684eba3ad1b2
jchassaing <jeremie.chassaing@gmail.com>
2024-12-25 13:59:31 +0100

    Comment day07

--- day07.fsx
+++ day07.fsx
@@ -1,5 +1,6 @@
 open System
 
+// convert each line as a pair of total and list of values
 let input =
     IO.File.ReadAllLines("input/day07.txt")
     |> Array.map (fun l ->
@@ -9,25 +10,40 @@ let input =
         | _ -> failwith "Nope"
     )
 
-let test, values = input[0]
 
+// try to find operators recursively
+// test is the expected value
+// total is the current total of the evaluation
+// values is the remaining values to add or multiply
 let rec findOps test total values =
     match values with
     | [] ->
+        // we used all values, check if the total matches the test value
         total = test
     | h :: rest ->
+        // there is at least one value to add/mul
         if total > test then
+            // the total is already greater than test
+            // and we are not finished yet, and the total can only get bigger,
+            // so no chance to find an answer in this direction, give up
             false
         else
+            // start looking for solitions using * as the get to big very fast we can abandon quickly
+            // for those that don't abandon, tests the +
             findOps test (total*h) rest
             || findOps test (total+h) rest
 
+// check if the test can be validated from values
 let isValid (test,values)= findOps test 0L values
 
+// filter lines and sum them
 input
 |> Array.filter isValid
 |> Array.sumBy fst
 
+
+// Part 2
+// count the number of digits of x (by dividing recursively by 10)
 let rec digits' x d =
     if x = 0L then
         d
@@ -35,16 +51,19 @@ let rec digits' x d =
         digits'(x/10L) (d+1) 
 let digits x = digits' x 0
 
+// Add d zeros to x
 let rec addZ x d =
     if d = 0 then
         x
     else
         addZ (x*10L) (d-1)
 
+// the || orperator, (called ||/ here to avoid conflict with boolean or)
 let (||/) x y = 
      addZ x (digits y) + y
 
 
+// similar to findOps, but with the new operation
 let rec findOps2 test total values =
     match values with
     | [] ->
